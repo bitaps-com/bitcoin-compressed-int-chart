e2746e3e6e515e2abc0b007a5d14c84940931048
4tochka <admin@bitaps.com>
2019-06-25 16:36:23 +0400

    initial commit

--- app/db_model/__init__.py
+++ app/db_model/__init__.py
@@ -1,4 +1,6 @@
 async def create_db_model(app, conn):
     await conn.execute(open("./db_model/sql/schema.sql", "r", encoding='utf-8').read().replace("\n",""))
     app.start_block = await conn.fetchval("SELECT height FROM blocks ORDER BY height DESC LIMIT 1;")
+    if app.start_block is None:
+        app.start_block = 0
 
